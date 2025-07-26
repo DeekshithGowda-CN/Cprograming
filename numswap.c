40b58bedbde5867408ccda5ec857acdf8ccfe6d0
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-07-26 22:11:46 +0530

    Printing and scaning for Second number is done

--- numswap.c
+++ numswap.c
@@ -2,4 +2,7 @@
 int main() {
 	double first, second, temp;
 	printf("Enter first number: ");
-  scanf("%lf", &first);
+        scanf("%lf", &first);
+        printf("Enter second number: ");
+        scanf("%lf", &second);
+
