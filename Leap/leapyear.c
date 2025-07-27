2d6d6067c867a2532c6ecc42cdd093cc109d0bf7
Deekshith Gowda CN <deekshith.n2@s.amity.edu>
2025-07-27 21:04:31 +0530

    If else is used , int year is given

--- Leap/leapyear.c
+++ Leap/leapyear.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
 int main() {
+   int year;
+   printf("Enter a Year: ");
+   scanf("%d", &year);
+
+   if (year % 400 == 0) {
+	   printf("%d is a leap year. year");
+   }
+   else if (year % 100 == 0) {
+	   printf("%d is not a leap year. ");
+   }
 
